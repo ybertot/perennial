62fe6fde1c5b8c065476af90ea6c7dc19872454c
Lily Tsai <tslilyai@mit.edu>
2019-11-18 11:30:16 -0500

    finish update specs, but this cannot chain together failures (none locs) with regenerating locs when they have crashed

--- src/Examples/DistributedCtr/DistCtrImpl.v
+++ src/Examples/DistributedCtr/DistCtrImpl.v
@@ -28,19 +28,18 @@ Definition update_replica: val :=
     match: "replica2" with
     SOME "r2" =>
       let: "r1" := ref #0 in
-      "r2" <- !"r1"
          "r2" <- !"r2" + #1;;
         "r1" <- !"r2";;
         release "lk";;
         Pair (SOME "r1") (SOME "r2")
-    | NONE => "r1" <- ref #0;; "r2" <- ref #0
+    | NONE => let: "r1" := ref #0 in
+              let: "r2" := ref #0 in
         release "lk";;
         Pair (SOME "r1") (SOME "r2")
     end
   end.
 
-Definition get_replica: val :=
-  λ: "replica1" "replica2" "lk",
+Definition get_replica: val := λ: "replica1" "replica2" "lk",
   acquire "lk";;
   match: "replica1" with
     SOME "r1" => "r" <- !"r1"
@@ -98,14 +97,16 @@ Section proof.
     done.
   Qed.
 
-  Definition update_inv (mloc bloc : option loc) (γ1 γ2 : gname) : iProp Σ := ∃ n : Z,
+  Definition update_inv_exists (n: Z) (m b : loc) (γ1 γ2 : gname): iProp Σ :=
+      m ↦ #(n) ∗ b ↦ #(n) ∗ own γ1 (● (Excl' n)) ∗ own γ2 (● (Excl' n))%I.
+
+  Definition update_inv (mloc bloc : option loc) (γ1 γ2 : gname): iProp Σ := ∃ n : Z,
       match mloc, bloc with
-      | Some m, Some b => m ↦ #(n) ∗ b ↦ #(n) ∗ own γ1 (● (Excl' n)) ∗ own γ2 (● (Excl' n))%I
-      | Some m, None => ∃ nloc, m ↦ #(n) ∗ nloc ↦ #(n) ∗ own γ1 (● (Excl' n)) ∗ own γ2 (● (Excl' n))%I
-      | None, Some b => ∃ nloc, nloc ↦ #(n) ∗ b ↦ #(n) ∗ own γ1 (● (Excl' n)) ∗ own γ2 (● (Excl' n))%I
-      | None, None => ∃ nloc1 nloc2, nloc1 ↦ #(n) ∗ nloc2 ↦ #(n) ∗ own γ1 (● (Excl' n)) ∗ own γ2 (● (Excl' n))%I
+      | Some m, Some b => update_inv_exists n b m γ1 γ2
+      | Some m, None => ∃ nloc, update_inv_exists n m nloc γ1 γ2 
+      | None, Some b => ∃ nloc, update_inv_exists n nloc b γ1 γ2 
+      | None, None => ∃ nloc1 nloc2, update_inv_exists n nloc1 nloc2 γ1 γ2 
       end.
-    (* we need these to tell the value of n *)
 
   (* Notes: loc = kind of like a Coq literal number, LitV (LitLoc loc) is an actual value in the language *)
 
@@ -128,7 +129,7 @@ Section proof.
     wp_alloc backup as "Hr2"; wp_let.
  
     wp_apply (newlock_spec LockN (update_inv (Some master) (Some backup) γ1 γ2) with "[Hr1 Hr2 Hγ1● Hγ2●]").
-    unfold update_inv; iFrame; auto.
+    unfold update_inv. unfold update_inv_exists. iFrame; auto.
 
     iExists 0. iFrame; auto.
     iIntros (lk γ) "HIsLk".
@@ -139,21 +140,18 @@ Section proof.
   Qed.
 
   Lemma update_replica_some_spec : ∀ n master backup lk (r1 r2 : loc),
-      {{{ ⌜ (master = SOMEV #r1 ∧ backup = SOME #r2)
-          ∨ (master = SOMEV #r1 ∧ backup = NONEV)
-          ∨ (backup = SOMEV #r2 ∧ master = NONEV) ⌝
-          ∗ value_duplicated n r1 r2 lk
-
+      {{{ ⌜ (master = SOMEV #r1 ∧ backup = SOME #r2) ⌝ 
+          ∗ value_duplicated n (Some r1) (Some r2) lk
       }}}
       update_replica master backup lk
       {{{ nr1 nr2, RET (SOMEV #nr1, SOMEV #nr2) ;
-          value_duplicated (n+1) nr1 nr2 lk
+          value_duplicated (n+1) (Some nr1) (Some nr2) lk
       }}}.
   Proof.
     iIntros (n master backup lk r1 r2 ϕ) "Hvaldup Hϕ".
     unfold update_replica.
-    iDestruct "Hvaldup" as ([[-> ->] | [[-> ->] | [-> ->]]]) "HInv";
-      iDestruct "HInv" as (γ γ1 γ2) "[#Hislk [Hγ1◯ Hγ2◯]]";
+    iDestruct "Hvaldup" as ([-> ->]) "HInv".
+    iDestruct "HInv" as (γ γ1 γ2) "[#Hislk [Hγ1◯ Hγ2◯]]";
       wp_pures;
       wp_apply (acquire_spec with "[$Hislk]");
       iIntros "(Hlked & HInv)";
@@ -162,36 +160,90 @@ Section proof.
         iDestruct (ghost_var_agree with "Hγ1● Hγ1◯") as %->;
         iMod (ghost_var_update γ1 (n+1) with "Hγ1● Hγ1◯") as "[Hγ1● Hγ1◯]";
         iMod (ghost_var_update γ2 (n+1) with "Hγ2● Hγ2◯") as "[Hγ2● Hγ2◯]".
-    - wp_load; wp_op; wp_store.
-      wp_load; wp_op; wp_store.
-      wp_apply (release_spec LockN γ lk (update_inv r1 r2 γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
+    wp_load; wp_op; wp_store.
+    wp_load; wp_op; wp_store.
+    wp_apply (release_spec LockN γ lk (update_inv (Some r1) (Some r2) γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
       iSplit; auto. unfold update_inv.
       iExists (n + 1). iFrame; auto.
       iIntros. wp_pures.
       iApply "Hϕ".
       iExists γ, γ1, γ2; iFrame; auto.
-    - wp_load; wp_op; wp_store.
-      wp_match.
-      wp_alloc r3 as "Hr3".
-      wp_let.
-      wp_load. wp_store.
-      wp_load. wp_store.
-      wp_load. 
-      wp_apply (release_spec LockN γ lk (update_inv r1 r2 γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
-      iSplit; auto. unfold update_inv.
-      iExists (n + 1). iFrame; auto.
-      iIntros. wp_pures.
-      iApply "Hϕ".
-      iExists γ, γ1, γ2; iFrame; auto.
-
   Qed.
- 
-  Lemma update_replica_none_spec : ∀ n master backup lk (r1 r2 : loc),
-      {{{ ⌜ master = NONE ∧ backup = SOMEV #r2 ⌝ ∗ value_duplicated n r1 r2 lk }}}
+
+  Lemma update_replica_some_none_spec : ∀ n master backup lk (r1: loc),
+      {{{ ⌜ (master = SOMEV #r1 ∧ backup = NONEV) ⌝
+            ∗ value_duplicated n (Some r1) None lk
+      }}}
       update_replica master backup lk
-      {{{ RET (SOMEV #r1, SOMEV #r2) ; value_duplicated (n+1) r1 r2 lk }}}.
+      {{{ nr1 nr2, RET (SOMEV #nr1, SOMEV #nr2) ;
+          value_duplicated (n+1) (Some nr1) None lk (* XXX: How can I make this be Some nr1 and Some nr2? *)
+      }}}.
+  Proof.
+    iIntros (n master backup lk r1 ϕ) "Hvaldup Hϕ".
+    unfold update_replica.
+    iDestruct "Hvaldup" as ([-> ->]) "HInv";
+      iDestruct "HInv" as (γ γ1 γ2) "[#Hislk [Hγ1◯ Hγ2◯]]";
+      wp_pures;
+      wp_apply (acquire_spec with "[$Hislk]");
+      iIntros "(Hlked & HInv)";
+      wp_pures;
+    iDestruct "HInv" as (n' nloc) "(Hr1 & Hr2 & Hγ1● & Hγ2● )";
+        iDestruct (ghost_var_agree with "Hγ1● Hγ1◯") as %->;
+        iMod (ghost_var_update γ1 (n+1) with "Hγ1● Hγ1◯") as "[Hγ1● Hγ1◯]";
+        iMod (ghost_var_update γ2 (n+1) with "Hγ2● Hγ2◯") as "[Hγ2● Hγ2◯]".
+    wp_load; wp_op; wp_store; wp_match; wp_alloc nr2 as "Hnr2"; wp_let; wp_load; wp_store.
+    wp_apply (release_spec LockN γ lk (update_inv (Some r1) None γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
+    iSplit; auto.
+    iExists (n + 1), nr2. iFrame; auto. 
+    iIntros. wp_pures.
+    iApply "Hϕ".
+    iExists γ, γ1, γ2; iFrame; auto.
+  Qed.
+
+  Lemma update_replica_none_some_spec : ∀ n master backup lk (r1 r2 : loc),
+      {{{ 
+           ⌜ (master = NONEV ∧ backup = SOMEV #r1) ⌝
+             ∗ value_duplicated n None (Some r1) lk
+      }}}
+      update_replica master backup lk
+      {{{ nr1 nr2, RET (SOMEV #nr1, SOMEV #nr2) ;
+          value_duplicated (n+1) None (Some nr2) lk (* XXX: How can I make this be Some nr1 and Some nr2? *)
+      }}}.
   Proof.
     iIntros (n master backup lk r1 r2 ϕ) "Hvaldup Hϕ".
+    unfold update_replica.
+    iDestruct "Hvaldup" as ([-> ->]) "HInv".
+    iDestruct "HInv" as (γ γ1 γ2) "[#Hislk [Hγ1◯ Hγ2◯]]";
+      wp_pures;
+      wp_apply (acquire_spec with "[$Hislk]");
+      iIntros "(Hlked & HInv)";
+      wp_pures.
+    iDestruct "HInv" as (n' nloc) "(Hr1 & Hr2 & Hγ1● & Hγ2● )";
+        iDestruct (ghost_var_agree with "Hγ1● Hγ1◯") as %->;
+        iMod (ghost_var_update γ1 (n+1) with "Hγ1● Hγ1◯") as "[Hγ1● Hγ1◯]";
+        iMod (ghost_var_update γ2 (n+1) with "Hγ2● Hγ2◯") as "[Hγ2● Hγ2◯]".
+    - wp_alloc nr2 as "Hnr2"; wp_let; wp_load; wp_store.
+      wp_load; wp_store.
+      wp_apply (release_spec LockN γ lk (update_inv None (Some r1) γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
+      iSplit; auto.
+      iExists (n + 1), nr2. iFrame; auto. 
+      iIntros. wp_pures.
+      iApply "Hϕ".
+      iExists γ, γ1, γ2; iFrame; auto.
+  Qed.
+ 
+  Lemma update_replica_none_spec : ∀ master backup lk, 
+      {{{ ⌜ master = NONE ∧ backup = NONE ⌝
+           ∗ value_duplicated 0 None None lk }}}
+      update_replica master backup lk
+      {{{ r1 r2, RET (SOMEV #r1, SOMEV #r2) ;
+          value_duplicated (0) None None lk
+          (*XXX I want to say value_duplicated (0) (Some r1) (Some r2) lk, but I can't because
+             the lock invariant can't be changed
+           *)
+      }}}.
+  Proof.
+    iIntros (master backup lk ϕ) "Hvaldup Hϕ".
     unfold update_replica.
     wp_pures.
     iDestruct "Hvaldup" as ([-> ->]) "HInv".
@@ -202,58 +254,17 @@ Section proof.
     iIntros "(Hlked & HInv)".
     wp_pures.
 
-    iDestruct "HInv" as (n') "(Hr1 & Hr2 & Hγ1● & Hγ2● )".
-    iDestruct (ghost_var_agree with "Hγ1● Hγ1◯") as %->.
-    iMod (ghost_var_update γ1 (n+1) with "Hγ1● Hγ1◯") as "[Hγ1● Hγ1◯]".
-    iMod (ghost_var_update γ2 (n+1) with "Hγ2● Hγ2◯") as "[Hγ2● Hγ2◯]".
+    iDestruct "HInv" as (n' nloc nloc2) "(Hnloc1 & Hnloc2 & Hγ1● & Hγ2● )";
+      iDestruct (ghost_var_agree with "Hγ1● Hγ1◯") as %->.
 
-    wp_load; wp_op; wp_store.
-    wp_load; wp_op; wp_store.
-    wp_apply (release_spec LockN γ lk (update_inv r1 r2 γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
+    wp_alloc r1 as "Hr1". 
+    wp_alloc r2 as "Hr2".
+    wp_apply (release_spec LockN γ lk (update_inv None None γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
     iSplit; auto. unfold update_inv.
-    iExists (n + 1). iFrame; auto.
-
+    iExists 0, nloc, nloc2. iFrame; auto.
     iIntros. wp_pures.
     iApply "Hϕ".
     iExists γ, γ1, γ2; iFrame; auto.
   Qed.
-  
-  Lemma update_spec : ∀ n r1 r2 lk, (*should γ be forall? *)
-    {{{ value_duplicated n r1 r2 lk }}}
-      update #r1 #r2 lk
-    {{{ RET #() ; value_duplicated (n+1) r1 r2 lk }}}.
-  Proof.
-    iIntros (n r1 r2 lk ϕ) "Hvaldup Hϕ".
-    unfold update.
-    wp_pures.
-    unfold value_duplicated in *.
-    iDestruct "Hvaldup" as (γ γ1 γ2) "[Hislk [Hγ1◯ Hγ2◯]]".
-    wp_apply (acquire_spec with "[Hislk]"). iApply "Hislk".
-    iIntros "(Hlked & HInv)".
-    wp_pures.
-    unfold update_replica. wp_pures.
-    induction r1.
-    induction r1.
-    iDestruct #r1 as "HR1". simpl.
-    iDestruct "HInv" as "(_ & Hr1 & Hr2 & Hγ1● & Hγ2● )"; simpl in *.
-    wp_apply (wp_par (λ _, own γ1 (◯ Excl' (n+1))) (λ _, own γ2 (◯ Excl' (n+1)))
-                with "[Hr1 Hγ1◯ Hγ1●] [Hr2 Hγ2◯ Hγ2●]").
-    - iMod (ghost_var_update γ1 (n+1) with "Hγ1● Hγ1◯") as "[Hγ1● Hγ1◯]".
-      wp_load; wp_op; wp_store; auto.
-    - iMod (ghost_var_update γ2 (n+1) with "Hγ2● Hγ2◯") as "[Hγ2● Hγ2◯]".
-      wp_load; wp_op; wp_store; auto.
-    - iIntros (v1 v2) "(Hγ1◯ & Hγ2◯)".
-      iNext.
-      wp_pures.
-      Check release_spec.
 
-      auto.
-  Admitted.
-
-  Lemma sum_spec : ∀ n rpair lpair,
-    {{{ pair_eq n rpair }}}
-      sum (#(fst rpair), #(snd rpair)) lpair
-    {{{ z, RET #z; ⌜ z = (n + n)%Z ⌝ }}}.
-  Proof.
-  Admitted.
 End proof.
