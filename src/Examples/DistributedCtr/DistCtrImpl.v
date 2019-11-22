563edc483a0550e7b75621983cdf9e79ea13b3a8
Lily Tsai <tslilyai@mit.edu>
2019-11-22 15:47:37 -0500

    add specs for update_replicas

--- src/Examples/DistributedCtr/DistCtrImpl.v
+++ src/Examples/DistributedCtr/DistCtrImpl.v
@@ -12,11 +12,10 @@ Definition update_node: val :=
   λ: "node",
   match: !"node" with
     SOME "v1" => "node" <- SOME ("v1" + #1);;
-                        "r" <- #1
+                        #1
   | NONE => "node" <- SOME #0;;
-                   "r" <- #0
-  end;;
-  "r".
+                   #0
+  end.
 
 Definition kill_node: val :=
   λ: "node",
@@ -32,13 +31,13 @@ Definition get_replicas : val :=
   λ: "node1" "node2",
   match: !"node1" with
     SOME "v1" => "node2" <- "v1";;
-                         "r" <- "v1"
+                         "v1"
     | NONE => match: !"node2" with
                 SOME "v2" => "node1" <- "v2";;
-                               "r" <- "v2"
+                               "v2"
               | NONE => "node1" <- SOME #0;;
                                 "node2" <- SOME #0;;
-                                "r" <- #0
+                                #-1
               end
   end;;
   "r".
@@ -141,140 +140,120 @@ Section proof.
     iFrame.
   Qed.
 
-  Lemma update_replica_some_spec : ∀ n master backup lk v1 v2 γ γ1 γ2,
+  Lemma update_node_some_spec : ∀ (n: Z) γ1 node,
       {{{
-           lock_inv lk γ γ1 γ2
-           ∗ value_duplicated_inv n (Some v1) (Some v2) γ1 γ2
-           ∗ master ↦ SOMEV #v1
-           ∗ backup ↦ SOMEV #v2
-      }}}
-        update_replicas #master #backup lk
-      {{{ RET Pair #master #backup; 
-           lock_inv lk γ γ1 γ2
-           ∗ value_duplicated_inv (n+1) (Some (v1+1)) (Some (v2+1)) γ1 γ2
-           ∗ master ↦ SOMEV #(v1 + 1)
-           ∗ backup ↦ SOMEV #(v2 + 1)
-      }}}.
+           node ↦ SOMEV #n
+           ∗ node_lock_inv n γ1
+           ∗ node_val_inv n (Some n) γ1
+    }}}
+      update_node #node
+    {{{ RET #1;
+          node ↦ SOMEV #(n+1)
+          ∗ node_lock_inv (n+1) γ1
+          ∗ node_val_inv (n+1) (Some (n+1)) γ1
+    }}}.
   Proof.
-    iIntros (n master backup lk v1 v2 γ γ1 γ2 ϕ) "Hvaldup Hϕ".
-    unfold update_replicas.
-    iDestruct "Hvaldup" as "(#Hlk & HInv & Hm & Hb)".
-    unfold value_duplicated_inv.
-    iDestruct "HInv" as "[Hv1 [Hγ1◯ [Hv2 Hγ2◯]]]".
-
+    iIntros (n γ1 node Φ) "(Hnode & Hlkinv & Hnodeinv & Hown) HPost".
+    unfold update_node.
+    unfold node_lock_inv.
+    iMod (ghost_var_update γ1 (n+1) with "Hlkinv Hown") as "[Hγ1● Hγ1◯]".
     wp_pures.
-    wp_apply (acquire_spec with "Hlk").
-    iIntros "(Hlked & HInv)".
-    iDestruct "HInv" as (n') "(Hγ1● & Hγ2● )".
-    iDestruct (ghost_var_agree with "Hγ1● Hγ1◯") as %->.
-    iMod (ghost_var_update γ1 (n+1) with "Hγ1● Hγ1◯") as "[Hγ1● Hγ1◯]";
-    iMod (ghost_var_update γ2 (n+1) with "Hγ2● Hγ2◯") as "[Hγ2● Hγ2◯]".
+    wp_load.
     wp_pures.
-    wp_load. wp_match. wp_store. wp_store.
-    wp_apply (release_spec LockN γ lk (update_val_inv γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hv1 Hv2 Hm Hb Hϕ]").
-    iSplit; auto. unfold update_val_inv.
-    iExists (n+1). iFrame; auto.
-    iIntros. wp_pures.
-    unfold lock_inv.
-    iDestruct "Hϕ" as (
-    iApply "Hϕ".
-      iExists γ, γ1, γ2; iFrame; auto.
+    wp_store.
+    iApply "HPost". 
+    iFrame; auto.
   Qed.
 
-  Lemma update_replica_some_none_spec : ∀ n master backup lk (r1: loc),
-      {{{ ⌜ (master = SOMEV #r1 ∧ backup = NONEV) ⌝
-            ∗ value_duplicated n (Some r1) None lk
-      }}}
-      update_replica master backup lk
-      {{{ nr1 nr2, RET (SOMEV #nr1, SOMEV #nr2) ;
-          value_duplicated (n+1) (Some nr1) None lk (* XXX: How can I make this be Some nr1 and Some nr2? *)
-      }}}.
+  Lemma update_node_none_spec : ∀ (n: Z) γ1 node,
+      {{{
+           node ↦ NONEV
+           ∗ node_lock_inv n γ1
+           ∗ node_val_inv n None γ1
+    }}}
+      update_node #node
+    {{{ RET #0;
+          node ↦ SOMEV #0
+          ∗ node_lock_inv 0 γ1
+          ∗ node_val_inv 0 (Some 0) γ1
+    }}}.
   Proof.
-    iIntros (n master backup lk r1 ϕ) "Hvaldup Hϕ".
-    unfold update_replica.
-    iDestruct "Hvaldup" as ([-> ->]) "HInv";
-      iDestruct "HInv" as (γ γ1 γ2) "[#Hislk [Hγ1◯ Hγ2◯]]";
-      wp_pures;
-      wp_apply (acquire_spec with "[$Hislk]");
-      iIntros "(Hlked & HInv)";
-      wp_pures;
-    iDestruct "HInv" as (n' nloc) "(Hr1 & Hr2 & Hγ1● & Hγ2● )";
-        iDestruct (ghost_var_agree with "Hγ1● Hγ1◯") as %->;
-        iMod (ghost_var_update γ1 (n+1) with "Hγ1● Hγ1◯") as "[Hγ1● Hγ1◯]";
-        iMod (ghost_var_update γ2 (n+1) with "Hγ2● Hγ2◯") as "[Hγ2● Hγ2◯]".
-    wp_load; wp_op; wp_store; wp_match; wp_alloc nr2 as "Hnr2"; wp_let; wp_load; wp_store.
-    wp_apply (release_spec LockN γ lk (update_inv (Some r1) None γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
-    iSplit; auto.
-    iExists (n + 1), nr2. iFrame; auto. 
-    iIntros. wp_pures.
-    iApply "Hϕ".
-    iExists γ, γ1, γ2; iFrame; auto.
-  Qed.
-
-  Lemma update_replica_none_some_spec : ∀ n master backup lk (r1 r2 : loc),
-      {{{ 
-           ⌜ (master = NONEV ∧ backup = SOMEV #r1) ⌝
-             ∗ value_duplicated n None (Some r1) lk
-      }}}
-      update_replica master backup lk
-      {{{ nr1 nr2, RET (SOMEV #nr1, SOMEV #nr2) ;
-          value_duplicated (n+1) None (Some nr2) lk (* XXX: How can I make this be Some nr1 and Some nr2? *)
-      }}}.
-  Proof.
-    iIntros (n master backup lk r1 r2 ϕ) "Hvaldup Hϕ".
-    unfold update_replica.
-    iDestruct "Hvaldup" as ([-> ->]) "HInv".
-    iDestruct "HInv" as (γ γ1 γ2) "[#Hislk [Hγ1◯ Hγ2◯]]";
-      wp_pures;
-      wp_apply (acquire_spec with "[$Hislk]");
-      iIntros "(Hlked & HInv)";
-      wp_pures.
-    iDestruct "HInv" as (n' nloc) "(Hr1 & Hr2 & Hγ1● & Hγ2● )";
-        iDestruct (ghost_var_agree with "Hγ1● Hγ1◯") as %->;
-        iMod (ghost_var_update γ1 (n+1) with "Hγ1● Hγ1◯") as "[Hγ1● Hγ1◯]";
-        iMod (ghost_var_update γ2 (n+1) with "Hγ2● Hγ2◯") as "[Hγ2● Hγ2◯]".
-    - wp_alloc nr2 as "Hnr2"; wp_let; wp_load; wp_store.
-      wp_load; wp_store.
-      wp_apply (release_spec LockN γ lk (update_inv None (Some r1) γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
-      iSplit; auto.
-      iExists (n + 1), nr2. iFrame; auto. 
-      iIntros. wp_pures.
-      iApply "Hϕ".
-      iExists γ, γ1, γ2; iFrame; auto.
-  Qed.
- 
-  Lemma update_replica_none_spec : ∀ master backup lk, 
-      {{{ ⌜ master = NONE ∧ backup = NONE ⌝
-           ∗ value_duplicated 0 None None lk }}}
-      update_replica master backup lk
-      {{{ r1 r2, RET (SOMEV #r1, SOMEV #r2) ;
-          value_duplicated (0) None None lk
-          (*XXX I want to say value_duplicated (0) (Some r1) (Some r2) lk, but I can't because
-             the lock invariant can't be changed
-           *)
-      }}}.
-  Proof.
-    iIntros (master backup lk ϕ) "Hvaldup Hϕ".
-    unfold update_replica.
+    iIntros (n γ1 node Φ) "(Hnode & Hlkinv & Hnodeinv & Hown) HPost".
+    unfold update_node.
+    unfold node_lock_inv.
+    iMod (ghost_var_update γ1 0 with "Hlkinv Hown") as "[Hγ1● Hγ1◯]".
     wp_pures.
-    iDestruct "Hvaldup" as ([-> ->]) "HInv".
-    unfold value_duplicated in *.
-    iDestruct "HInv" as (γ γ1 γ2) "[#Hislk [Hγ1◯ Hγ2◯]]".
-
-    wp_apply (acquire_spec with "[$Hislk]").
-    iIntros "(Hlked & HInv)".
+    wp_load.
     wp_pures.
-
-    iDestruct "HInv" as (n' nloc nloc2) "(Hnloc1 & Hnloc2 & Hγ1● & Hγ2● )";
-      iDestruct (ghost_var_agree with "Hγ1● Hγ1◯") as %->.
-
-    wp_alloc r1 as "Hr1". 
-    wp_alloc r2 as "Hr2".
-    wp_apply (release_spec LockN γ lk (update_inv None None γ1 γ2) with "[- $Hlked Hγ1◯ Hγ2◯ Hϕ]").
-    iSplit; auto. unfold update_inv.
-    iExists 0, nloc, nloc2. iFrame; auto.
-    iIntros. wp_pures.
-    iApply "Hϕ".
-    iExists γ, γ1, γ2; iFrame; auto.
+    wp_store.
+    iApply "HPost". 
+    iFrame; auto.
   Qed.
+
+  Lemma update_replicas_both_alive_spec : ∀ (n: Z) γ1 γ2 γ3 γ4 lk1 lk2 node1 node2,
+      {{{
+           node1 ↦ SOMEV #n 
+           ∗ node2 ↦ SOMEV #n
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv n γ1)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv n γ1)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv n γ2)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv n γ2)
+           ∗ global_inv n (Some n) (Some n) γ1 γ2
+    }}}
+      update_replicas #node1 lk1 #node2 lk2
+    {{{ RET #();
+           node1 ↦ SOMEV #(n+1) 
+           ∗ node2 ↦ SOMEV #(n+1)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv (n+1) γ1)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv (n+1) γ1)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv (n+1) γ2)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv (n+1) γ2)
+           ∗ global_inv (n+1) (Some (n+1)) (Some (n+1)) γ1 γ2
+    }}}.
+  Proof.
+  Admitted.
+
+  Lemma update_replicas_one_dead_spec : ∀ (n: Z) γ1 γ2 γ3 γ4 lk1 lk2 node1 node2,
+      {{{
+           (node1 ↦ NONEV ∗ node2 ↦ SOMEV #n ∨ 
+            node1 ↦ SOMEV #n ∗ node2 ↦ NONEV)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv n γ1)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv n γ1)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv n γ2)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv n γ2)
+           ∗ global_inv n (Some n) (Some n) γ1 γ2
+    }}}
+      update_replicas #node1 lk1 #node2 lk2
+    {{{ RET #();
+           node1 ↦ SOMEV #(n+1) ∗ node2 ↦ SOMEV #(n+1)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv (n+1) γ1)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv (n+1) γ1)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv (n+1) γ2)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv (n+1) γ2)
+           ∗ global_inv (n+1) (Some (n+1)) (Some (n+1)) γ1 γ2
+    }}}.
+  Proof.
+  Admitted.
+
+  Lemma update_replicas_both_dead_spec : ∀ (n: Z) γ1 γ2 γ3 γ4 lk1 lk2 node1 node2,
+      {{{
+           (node1 ↦ NONEV ∗ node2 ↦ NONEV)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv n γ1)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv n γ1)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv n γ2)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv n γ2)
+           ∗ global_inv n (Some n) (Some n) γ1 γ2
+    }}}
+      update_replicas #node1 lk1 #node2 lk2
+    {{{ RET #();
+           node1 ↦ SOMEV #(0) ∗ node2 ↦ SOMEV #(0)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv (0) γ1)
+           ∗ is_lock LockN γ3 lk1 (node_lock_inv (0) γ1)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv (0) γ2)
+           ∗ is_lock LockN γ4 lk2 (node_lock_inv (0) γ2)
+           ∗ global_inv (0) (Some 0) (Some 0) γ1 γ2
+    }}}.
+  Proof.
+  Admitted.
+
 End proof.
